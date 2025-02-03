3ce32ec0d182f33de0526c76991d55f6230c58cf
Michael-Feleke <felekemichael999@gmail.com>
2025-02-03 23:26:05 +0300

    Update NormalNav component to include Teams page in navigation logic

--- src/features/header/NormalNav.jsx
+++ src/features/header/NormalNav.jsx
@@ -8,44 +8,49 @@ import FeaturesNav from "./FeaturesNav";
 import ResourceNav from "./ResourceNav";
 
 const NormalNav = ({ isHome }) => {
-	const navigateTo = useNavigate();
-	const { isUserSignedIn } = useSelector((state) => state.auth);
-	const { goAnonymously } = useSelector((state) => state.community);
-	const [isSearching, setIsSearching] = useState(false);
-	const { pathname } = useLocation();
-	const hasSearchBar =
-		pathname === "/community" && (isUserSignedIn || goAnonymously);
-	const showHomeNav =
-		pathname.includes("/community") || pathname.includes("/profile");
-	const showCommNav = pathname === "/" || pathname.includes("/profile");
+  const navigateTo = useNavigate();
+  const { isUserSignedIn } = useSelector((state) => state.auth);
+  const { goAnonymously } = useSelector((state) => state.community);
+  const [isSearching, setIsSearching] = useState(false);
+  const { pathname } = useLocation();
+  const hasSearchBar =
+    pathname === "/community" && (isUserSignedIn || goAnonymously);
+  const showHomeNav =
+    pathname.includes("/community") ||
+    pathname.includes("/profile") ||
+    pathname.includes("/teams");
+  const showCommNav =
+    pathname === "/" ||
+    pathname.includes("/profile") ||
+    pathname.includes("/teams");
 
-	const handleClick = (href) => navigateTo(href);
+  const handleClick = (href) => navigateTo(href);
 
-	return (
-		<nav className="flex sm:items-center md:justify-center md:gap-3 sm:gap-1 md:flex-grow">
-			{!isSearching && (
-				<div className="md:flex items-center md:gap-3 sm:gap-1 hidden">
-					{showHomeNav && (
-						<NavBtn href={""} onClick={() => handleClick("/")}>
-							Home
-						</NavBtn>
-					)}
-					{showCommNav && (
-						<NavBtn href={""} onClick={() => handleClick("/community")}>
-							Community
-						</NavBtn>
-					)}
-					<NavBtn href={""} onClick={() => handleClick("/editor/dotcode")}>
-						Editor
-					</NavBtn>
-					{isHome && <FeaturesNav />}
-					<ResourceNav />
-					<DevelopersNav />
-				</div>
-			)}
-			{hasSearchBar && <Search setIsSearching={setIsSearching} />}
-		</nav>
-	);
+  return (
+    <nav className="flex sm:items-center md:justify-center md:gap-3 sm:gap-1 md:flex-grow">
+      {!isSearching && (
+        <div className="md:flex items-center md:gap-3 sm:gap-1 hidden">
+          {showHomeNav && (
+            <NavBtn href={""} onClick={() => handleClick("/")}>
+              Home
+            </NavBtn>
+          )}
+          {showCommNav && (
+            <NavBtn href={""} onClick={() => handleClick("/community")}>
+              Community
+            </NavBtn>
+          )}
+          <NavBtn href={""} onClick={() => handleClick("/editor/dotcode")}>
+            Editor
+          </NavBtn>
+          {isHome && <FeaturesNav />}
+          <ResourceNav />
+          <DevelopersNav />
+        </div>
+      )}
+      {hasSearchBar && <Search setIsSearching={setIsSearching} />}
+    </nav>
+  );
 };
 
 export default NormalNav;
