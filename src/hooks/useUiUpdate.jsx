d7bdeb320a109ee62727327fe2c2e7ffb6606063
tsegaye27 <ttalegn29@gmail.com>
2025-02-08 19:46:49 +0300

    updated useUiUpdate

--- src/hooks/useUiUpdate.jsx
+++ src/hooks/useUiUpdate.jsx
@@ -2,18 +2,34 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateSelectedLng } from "../pages/editor/projectSlice";
 
 export const useUiUpdate = () => {
-	const { project } = useSelector((state) => state.project);
-	const dispatch = useDispatch();
+  const { project, selectedVersion } = useSelector((state) => state.project);
+  const dispatch = useDispatch();
 
-	const handleSelection = (lngName) => {
-		if (lngName === "html") {
-			dispatch(updateSelectedLng({ code: project.code.html, lng: lngName }));
-		} else if (lngName === "css") {
-			dispatch(updateSelectedLng({ code: project.code.css, lng: lngName }));
-		} else {
-			dispatch(updateSelectedLng({ code: project.code.js, lng: lngName }));
-		}
-	};
+  const handleSelection = (lngName) => {
+    console.log(project.code, selectedVersion, lngName);
+    if (lngName === "html") {
+      dispatch(
+        updateSelectedLng({
+          code: project.code[selectedVersion - 1].html,
+          lng: lngName,
+        }),
+      );
+    } else if (lngName === "css") {
+      dispatch(
+        updateSelectedLng({
+          code: project.code[selectedVersion - 1].css,
+          lng: lngName,
+        }),
+      );
+    } else {
+      dispatch(
+        updateSelectedLng({
+          code: project.code[selectedVersion - 1].js,
+          lng: lngName,
+        }),
+      );
+    }
+  };
 
-	return handleSelection;
+  return handleSelection;
 };
