f920f036ed97fba6cc22e91f44b8dd83a8be7ea9
Michael-Feleke <felekemichael999@gmail.com>
2025-02-09 18:41:17 +0300

    feat: implement reset password saga for handling password reset requests

---
+++ src/features/auth/resetPassword/resetPasswordSaga.js
@@ -0,0 +1,30 @@
+import { takeLatest } from "redux-saga/effects";
+import {
+  resetPasswordFailure,
+  resetPasswordRequest,
+  resetPasswordSuccess,
+} from "./resetPasswordSlice";
+
+// Saga worker function
+function* workResetPasswordSaga(action) {
+  try {
+    const { token, password, confirmPassword } = action.payload;
+    yield call(
+      axios.post,
+      `${
+        import.meta.env.VITE_REACT_APP_API_URL
+      }/api/v1/users/resetpassword/${token}`,
+      { password, confirmPassword },
+      { withCredentials: true }
+    );
+    yield put(resetPasswordSuccess());
+  } catch (error) {
+    const message = error.response?.data?.message || error.message;
+    yield put(resetPasswordFailure(message));
+  }
+}
+
+// Saga watcher function
+export function* watchResetPasswordSaga() {
+  yield takeLatest(resetPasswordRequest.type, workResetPasswordSaga);
+}
