35d0012611ca468e2f14bc0e957bc5c092947524
Michael-Feleke <felekemichael999@gmail.com>
2025-02-09 16:07:42 +0300

    refactor: improve user data handling and UI updates in Account component

--- src/features/auth/authData.js
+++ src/features/auth/authData.js
@@ -1,41 +1,42 @@
 import Cookies from "js-cookie";
 
 export const storeUserData = (user) => {
-	const userData = user.data;
-	const jwt = user.token;
-	const expiration = Date.now() + 24 * 60 * 60 * 1000;
-	const data = {
-		loginAt: Date.now(),
-		expiry: expiration,
-		name: userData.name,
-		userId: userData._id,
-		email: userData.email,
-		bio: userData.bio,
-		avatarUrl: userData.avatarUrl,
-	};
+  const userData = user;
+  console.log({ user });
+  const jwt = user.token;
+  const expiration = Date.now() + 24 * 60 * 60 * 1000;
+  const data = {
+    loginAt: Date.now(),
+    expiry: expiration,
+    name: userData.name,
+    userId: userData._id,
+    email: userData.email,
+    bio: userData.bio,
+    avatarUrl: userData.avatarUrl,
+  };
 
-	Cookies.set("jwt", jwt, { expires: 1000 * 60 * 60 * 24 });
+  Cookies.set("jwt", jwt, { expires: 1000 * 60 * 60 * 24 });
 
-	localStorage.setItem("userData", JSON.stringify(data));
+  localStorage.setItem("userData", JSON.stringify(data));
 };
 
 export const getUserData = (isToken = false) => {
-	const storedData = JSON.parse(localStorage.getItem("userData"));
-	const token = Cookies.get("jwt");
+  const storedData = JSON.parse(localStorage.getItem("userData"));
+  const token = Cookies.get("jwt");
 
-	if (storedData && token && storedData.expiry > Date.now()) {
-		if (isToken) {
-			return token;
-		} else {
-			return storedData;
-		}
-	}
+  if (storedData && token && storedData.expiry > Date.now()) {
+    if (isToken) {
+      return token;
+    } else {
+      return storedData;
+    }
+  }
 
-	localStorage.removeItem("userData");
-	return null;
+  localStorage.removeItem("userData");
+  return null;
 };
 
 export const removeUserData = () => {
-	localStorage.removeItem("userData");
-	document.location.reload();
+  localStorage.removeItem("userData");
+  document.location.reload();
 };
