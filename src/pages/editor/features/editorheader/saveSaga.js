13b373906d85110ec5df5878e954fd17edd41ad3
tsegaye27 <ttalegn29@gmail.com>
2025-02-09 17:28:13 +0300

    made some changes

--- src/pages/editor/features/editorheader/saveSaga.js
+++ src/pages/editor/features/editorheader/saveSaga.js
@@ -7,35 +7,36 @@ import { setCurrProject } from "../../projectSlice";
 import { saveFailure, saveRequest, saveSuccess } from "./saveSlice";
 
 function* workSaveSaga(action) {
-	const token = getUserData(true);
-	try {
-		const response = yield call(
-			axios.post,
-			`${import.meta.env.VITE_REACT_APP_API_URL}/api/v1/projects`,
-			action.payload,
-			{
-				withCredentials: true,
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			}
-		);
-		yield put(changeMade());
-		yield put(setCurrProject({ isNew: false, project: response.data.doc }));
-		yield put(saveSuccess(response.data.doc));
-		yield put(setNotifier({ success: "Project published successfully!" }));
-	} catch (error) {
-		const message = error.response
-			? error.response.data
-				? error.response.data.message
-				: error.message
-			: error.message;
-		yield put(saveFailure(message));
-	}
+  const token = getUserData(true);
+  console.log(action.payload);
+  try {
+    const response = yield call(
+      axios.post,
+      `${import.meta.env.VITE_REACT_APP_API_URL}/api/v1/projects`,
+      action.payload,
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    yield put(changeMade());
+    yield put(setCurrProject({ isNew: false, project: response.data.doc }));
+    yield put(saveSuccess(response.data.doc));
+    yield put(setNotifier({ success: "Project published successfully!" }));
+  } catch (error) {
+    const message = error.response
+      ? error.response.data
+        ? error.response.data.message
+        : error.message
+      : error.message;
+    yield put(saveFailure(message));
+  }
 }
 
 function* watchSaveSagas() {
-	yield takeLatest(saveRequest.type, workSaveSaga);
+  yield takeLatest(saveRequest.type, workSaveSaga);
 }
 
 export default watchSaveSagas;
