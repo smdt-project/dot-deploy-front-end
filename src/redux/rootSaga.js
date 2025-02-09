e301b15dcc084d30375ccaab94de7c28018a9f7d
Michael-Feleke <felekemichael999@gmail.com>
2025-02-09 18:44:58 +0300

    feat: add watchResetPasswordSaga to rootSaga for password reset handling

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,45 +1,49 @@
 import { all, fork } from "redux-saga/effects";
 import {
-	watchLogoutSagas,
-	watchUserDataSagas,
+  watchLogoutSagas,
+  watchUserDataSagas,
 } from "../features/auth/authSaga";
 import { watchSignInSagas } from "../features/auth/signin/signInSaga";
 import { watchSignUpSagas } from "../features/auth/signup/signUpSaga";
 import watchCommDataSagas, {
-	watchCommentSagas,
-	watchDetailingSagas,
-	watchLikeSagas,
+  watchCommentSagas,
+  watchDetailingSagas,
+  watchLikeSagas,
 } from "../pages/community/communitySaga";
 import watchPostCreateSagas, {
-	watchPostDeleteSagas,
-	watchPostUpdateSagas,
+  watchPostDeleteSagas,
+  watchPostUpdateSagas,
 } from "../pages/community/features/comSidebar/postSaga";
 import watchSaveSagas from "../pages/editor/features/editorheader/saveSaga";
 import watchProjectUpdateSaga from "../pages/editor/projectSaga";
 import watchProfileSaga, {
-	watchItemDeleteSaga,
+  watchItemDeleteSaga,
 } from "../pages/profile/profileSaga";
 import { watchSearchSaga } from "../features/search/searchSaga";
+// import watchInviteMemberSaga from "../pages/teams/teamSaga";
+import { watchResetPasswordSaga } from "../features/auth/resetPassword/resetPasswordSaga";
 
 function* rootSaga() {
-	yield all([
-		fork(watchSignUpSagas),
-		fork(watchSignInSagas),
-		fork(watchLogoutSagas),
-		fork(watchUserDataSagas),
-		fork(watchProfileSaga),
-		fork(watchSaveSagas),
-		fork(watchProjectUpdateSaga),
-		fork(watchCommDataSagas),
-		fork(watchLikeSagas),
-		fork(watchCommentSagas),
-		fork(watchDetailingSagas),
-		fork(watchPostCreateSagas),
-		fork(watchPostUpdateSagas),
-		fork(watchPostDeleteSagas),
-		fork(watchItemDeleteSaga),
-		fork(watchSearchSaga),
-	]);
+  yield all([
+    fork(watchSignUpSagas),
+    fork(watchSignInSagas),
+    fork(watchLogoutSagas),
+    fork(watchUserDataSagas),
+    fork(watchProfileSaga),
+    fork(watchSaveSagas),
+    fork(watchProjectUpdateSaga),
+    fork(watchCommDataSagas),
+    fork(watchLikeSagas),
+    fork(watchCommentSagas),
+    fork(watchDetailingSagas),
+    fork(watchPostCreateSagas),
+    fork(watchPostUpdateSagas),
+    fork(watchPostDeleteSagas),
+    fork(watchItemDeleteSaga),
+    fork(watchSearchSaga),
+    // fork(watchInviteMemberSaga),
+    fork(watchResetPasswordSaga),
+  ]);
 }
 
 export default rootSaga;
