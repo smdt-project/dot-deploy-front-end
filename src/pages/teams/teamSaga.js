a28ed1c0a04fa1bc3633c59736f72ee35ea70136
Michael-Feleke <felekemichael999@gmail.com>
2025-02-10 19:07:45 +0300

    feat: add fetchProjectsSaga to retrieve projects with authorization token

--- src/pages/teams/teamSaga.js
+++ src/pages/teams/teamSaga.js
@@ -4,11 +4,16 @@ import {
   inviteMemberRequest,
   inviteMemberSuccess,
   inviteMemberFailure,
+  setProjects,
+  fetchProjectsRequest,
+  fetchProjectFailure,
 } from "./teamsSlice";
 import { resetNotifier, setNotifier } from "../../ui/notifierSlice";
+import { getUserData } from "../../features/auth/authData";
 
 function* workInviteMemberSaga(action) {
   const { email, teamId } = action.payload;
+  const token = getUserData(true);
 
   try {
     yield put(resetNotifier());
@@ -22,10 +27,12 @@ function* workInviteMemberSaga(action) {
       { email },
       {
         withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
     );
 
-    yield put(inviteMemberSuccess(response.data.data.invitedMember));
     yield put(resetNotifier());
     yield put(setNotifier({ success: "Invitation sent successfully!" }));
   } catch (error) {
@@ -36,12 +43,80 @@ function* workInviteMemberSaga(action) {
       : error.message;
     yield put(inviteMemberFailure(message));
     yield put(resetNotifier());
+    yield put(setNotifier({ error: " Failed to send invitation " }));
+  }
+}
+
+function* fetchProjectsSaga(action) {
+  const token = getUserData(true);
+
+  try {
+    yield put(resetNotifier());
+    const response = yield call(
+      axios.get,
+      `${import.meta.env.VITE_REACT_APP_API_URL}/api/v1/organization/projects/${
+        action.payload
+      }`,
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    yield put(setProjects(response.data.data.docs));
+  } catch (error) {
+    const message = error.response
+      ? error.response.data
+        ? error.response.data.message
+        : error.message
+      : error.message;
+    yield put(fetchProjectFailure(message));
+    yield put(resetNotifier());
     yield put(setNotifier({ error: message }));
   }
 }
 
+// function* approveInvitationSaga(action) {
+//   const { teamId, invitationId } = action.payload;
+//   const token = getUserData(true);
+
+//   try {
+//     yield put(resetNotifier());
+//     yield put(setNotifier({ loading: "Approving invitation..." }));
+
+//     const response = yield call(
+//       axios.post,
+//       `${
+//         import.meta.env.VITE_REACT_APP_API_URL
+//       }/api/v1/organization/invitations/${invitationId}/${teamId}`,
+//       {},
+//       {
+//         withCredentials: true,
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       }
+//     );
+
+//     yield put(resetNotifier());
+//     yield put(setNotifier({ success: "Invitation approved successfully!" }));
+//   } catch (error) {
+//     const message = error.response
+//       ? error.response.data
+//         ? error.response.data.message
+//         : error.message
+//       : error.message;
+//     yield put(inviteMemberFailure(message));
+//     yield put(resetNotifier());
+//     yield put(setNotifier({ error: " Failed to approve invitation " }));
+//   }
+// }
+
 export function* watchInviteMemberSaga() {
   yield takeLatest(inviteMemberRequest.type, workInviteMemberSaga);
+  yield takeLatest(fetchProjectsRequest.type, fetchProjectsSaga);
 }
 
 export default watchInviteMemberSaga;
