ad2d359991303cf5a35f2eacd5ad0f1cc57ee4ad
sgc93 <smachewgedefaw@gmail.com>
2025-02-12 11:58:39 -0900

    render: display commit box conditionally

--- src/pages/editor/EditorPage.jsx
+++ src/pages/editor/EditorPage.jsx
@@ -2,18 +2,22 @@ import { Outlet } from "react-router-dom";
 import EditorFooter from "./EditorFooter";
 import EditorHeader from "./features/editorheader/EditorHeader";
 import EditorSidebar from "./features/sidebar/EditorSidebar";
+import { useSelector } from "react-redux";
+import CommitBox from "./CommitBox";
 
 const EditorPage = () => {
-	return (
-		<div className="relative flex-col transition-all duration-500">
-			<EditorHeader />
-			<div className="flex h-[92dvh]">
-				<EditorSidebar />
-				<Outlet />
-			</div>
-			<EditorFooter />
-		</div>
-	);
+  const { isCommititng } = useSelector((state) => state.project);
+  return (
+    <div className="relative flex-col transition-all duration-500">
+      <EditorHeader />
+      <div className="flex h-[92dvh]">
+        <EditorSidebar />
+        <Outlet />
+      </div>
+      {isCommititng && <CommitBox />}
+      <EditorFooter />
+    </div>
+  );
 };
 
 export default EditorPage;
