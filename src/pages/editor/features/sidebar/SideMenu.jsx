4c2830b90320ce1324b6475470282ec13f7c78f8
Michael-Feleke <felekemichael999@gmail.com>
2025-05-19 09:31:39 +0300

    Refactor SideMenu component for improved code formatting and readability

--- src/pages/editor/features/sidebar/SideMenu.jsx
+++ src/pages/editor/features/sidebar/SideMenu.jsx
@@ -2,26 +2,29 @@ import { useSelector } from "react-redux";
 import Explore from "./Explore";
 import Settings from "./Setting";
 import SideSearch from "./SideSearch";
+import Chat from "./Chat";
 
 const SideMenu = () => {
-	const { currTab, currTabTitle } = useSelector((state) => state.sidebar);
+  const { currTab, currTabTitle } = useSelector((state) => state.sidebar);
 
-	return (
-		<div className="bg-[#22252d] h-full w-full flex flex-col">
-			<div className="py-2 px-5 shadow-sm shadow-slate-700">
-				<span className=" uppercase text-slate-500 text-sm  line-clamp-1 ">
-					{currTabTitle}
-				</span>
-			</div>
-			{currTab === "explore" ? (
-				<Explore />
-			) : currTab === "search" ? (
-				<SideSearch />
-			) : (
-				currTab === "setting" && <Settings />
-			)}
-		</div>
-	);
+  return (
+    <div className="bg-[#22252d] h-full w-full flex flex-col">
+      <div className="py-2 px-5 shadow-sm shadow-slate-700">
+        <span className=" uppercase text-slate-500 text-sm  line-clamp-1 ">
+          {currTabTitle}
+        </span>
+      </div>
+      {currTab === "explore" ? (
+        <Explore />
+      ) : currTab === "search" ? (
+        <SideSearch />
+      ) : currTab === "chat" ? (
+        <Chat />
+      ) : (
+        currTab === "setting" && <Settings />
+      )}
+    </div>
+  );
 };
 
 export default SideMenu;
