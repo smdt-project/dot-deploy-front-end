6e20f33dff97f7db021718e5d003b252bb6c6686
tsegaye27 <ttalegn29@gmail.com>
2025-05-25 17:35:59 +0300

    feat(sidebar): add setActiveTab reducer to sidebarSlice

--- src/pages/editor/features/sidebar/sidebarSlice.js
+++ src/pages/editor/features/sidebar/sidebarSlice.js
@@ -1,21 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	currTab: "explore",
-	currTabTitle: "Explore",
+  currTab: "explore",
+  currTabTitle: "Explore",
 };
 
 const sidebarSlice = createSlice({
-	name: "sidebar",
-	initialState: initialState,
-	reducers: {
-		selectMenu: (state, action) => {
-			state.currTab = action.payload.name;
-			state.currTabTitle = action.payload.title;
-		},
-	},
+  name: "sidebar",
+  initialState: initialState,
+  reducers: {
+    selectMenu: (state, action) => {
+      state.currTab = action.payload.name;
+      state.currTabTitle = action.payload.title;
+    },
+    setActiveTab: (state, action) => {
+      state.currTab = action.payload.tab;
+      state.currTabTitle = action.payload.title;
+    },
+  },
 });
 
-export const { selectMenu } = sidebarSlice.actions;
-
+export const { selectMenu, setActiveTab } = sidebarSlice.actions;
 export default sidebarSlice.reducer;
