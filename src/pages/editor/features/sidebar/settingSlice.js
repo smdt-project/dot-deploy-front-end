52b5faac70d003efc578be621b3af14a6d5e4014
tsegaye27 <ttalegn29@gmail.com>
2025-05-27 22:43:02 +0300

    feat(editor): add ghost text enable/disable setting

--- src/pages/editor/features/sidebar/settingSlice.js
+++ src/pages/editor/features/sidebar/settingSlice.js
@@ -1,66 +1,72 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	codeFontSize: 24,
-	codeTabSize: 4,
-	autoSave: false,
-	closeBrackets: true,
-	lineNo: true,
-	holder: "",
-	foldGut: true,
-	notifyInterval: 60,
+  codeFontSize: 24,
+  codeTabSize: 4,
+  autoSave: false,
+  closeBrackets: true,
+  lineNo: true,
+  holder: "",
+  foldGut: true,
+  notifyInterval: 60,
+  ghostTextEnabled: true,
 };
 
 export const settingSlice = createSlice({
-	name: "setting",
-	initialState: initialState,
-	reducers: {
-		updateCodeFontSize: (state, action) => {
-			state.codeFontSize = action.payload;
-		},
-		updateCodeTabSize: (state, action) => {
-			state.codeTabSize = action.payload;
-		},
-		setAutoSave: (state) => {
-			state.autoSave = !state.autoSave;
-		},
-		setNotifyInterval: (state, action) => {
-			state.notifyInterval = action.payload;
-		},
-		setCloseBrackets: (state) => {
-			state.closeBrackets = !state.closeBrackets;
-		},
-		setLineNo: (state) => {
-			state.lineNo = !state.lineNo;
-		},
-		setFoldGutter: (state) => {
-			state.foldGut = !state.foldGut;
-		},
-		setPlaceholder: (state, action) => {
-			state.holder = action.payload;
-		},
-		resetSettings: (state) => {
-			state.codeFontSize = initialState.codeFontSize;
-			state.codeTabSize = 4;
-			state.autoSave = false;
-			state.closeBrackets = true;
-			state.lineNo = true;
-			state.holder = "";
-			state.foldGut = true;
-		},
-	},
+  name: "setting",
+  initialState: initialState,
+  reducers: {
+    updateCodeFontSize: (state, action) => {
+      state.codeFontSize = action.payload;
+    },
+    updateCodeTabSize: (state, action) => {
+      state.codeTabSize = action.payload;
+    },
+    setAutoSave: (state) => {
+      state.autoSave = !state.autoSave;
+    },
+    setNotifyInterval: (state, action) => {
+      state.notifyInterval = action.payload;
+    },
+    setCloseBrackets: (state) => {
+      state.closeBrackets = !state.closeBrackets;
+    },
+    setLineNo: (state) => {
+      state.lineNo = !state.lineNo;
+    },
+    setFoldGutter: (state) => {
+      state.foldGut = !state.foldGut;
+    },
+    setPlaceholder: (state, action) => {
+      state.holder = action.payload;
+    },
+    setGhostTextEnabled: (state) => {
+      state.ghostTextEnabled = !state.ghostTextEnabled;
+    },
+    resetSettings: (state) => {
+      state.codeFontSize = initialState.codeFontSize;
+      state.codeTabSize = 4;
+      state.autoSave = false;
+      state.closeBrackets = true;
+      state.lineNo = true;
+      state.holder = "";
+      state.foldGut = true;
+      state.ghostTextEnabled = true;
+    },
+  },
 });
 
 export const {
-	resetSettings,
-	updateCodeFontSize,
-	updateCodeTabSize,
-	setAutoSave,
-	setCloseBrackets,
-	setFoldGutter,
-	setLineNo,
-	setPlaceholder,
-	setNotifyInterval,
+  resetSettings,
+  updateCodeFontSize,
+  updateCodeTabSize,
+  setAutoSave,
+  setCloseBrackets,
+  setFoldGutter,
+  setLineNo,
+  setPlaceholder,
+  setNotifyInterval,
+  setGhostTextEnabled,
 } = settingSlice.actions;
 
 export default settingSlice.reducer;
